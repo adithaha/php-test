ccd40108df7d230a0e52520b1395e810f9695232
junior <juniorjbn@gmail.com>
2017-03-02 16:34:08 -0300

    maldita aspa

--- db_insert.php
+++ db_insert.php
@@ -13,7 +13,7 @@ $dbconn = pg_connect("host=$DBHOST dbname=$DBNAME user=$DBUSER password=$DBPASS"
         or die('Could not connect: ' . pg_last_error());
 
 //perform the insert using pg_query
-$result = pg_query($dbconn, "INSERT INTO '$TABLENAME'(data) VALUES('$data')");
+$result = pg_query($dbconn, "INSERT INTO $TABLENAME(data) VALUES('$data')");
 
 // Closing connection
 pg_close($dbconn);
